eacd51f1c806dd10a059c6729bdd3dee9d66401a
Jakob Borg <borg.jakob@gmail.com>
2019-11-08 17:37:09 +0100

    testing NN for regression, producing good r2 vs epochs

--- Project2/src/testing_NN_regression.py
+++ Project2/src/testing_NN_regression.py
@@ -26,19 +26,19 @@ data = data_df.to_numpy()
 output = data[:,0].reshape(-1,1)
 input_data = data[:,1:]
 
-hidden_neuron_list = [100,100]
-epochs = 1000
-runs = 5
+hidden_neuron_list = [100]
+epochs = 400
+runs = 10
 r2_test_runs = np.zeros((runs,epochs))
 r2_train_runs = np.zeros((runs,epochs))
 r2_end_test = np.zeros(runs)
 r2_end_train = np.zeros(runs)
 reg = Regression(hidden_activation='RELU')
-eta = 5 e-4
-lmbd = 0
+eta = 1e-3#1e-3
+lmbd = 0#1e-2
 
-for i in tqdm(range(runs)):
-    X_train, X_test, Y_train, Y_test = train_test_split(input_data, output,test_size=0.1)
+for run in tqdm(range(runs)):
+    X_train, X_test, Y_train, Y_test = train_test_split(input_data, output,test_size=0.2)
     Scaler = preprocessing.StandardScaler()
     X_train_scaled = Scaler.fit_transform(X_train)
     X_test_scaled = Scaler.transform(X_test)
@@ -54,21 +54,18 @@ for i in tqdm(range(runs)):
                         debug=False)
     
     nn.SGD(track=['r2'],test_data=(X_test_scaled,Y_test),train_data=(X_train_scaled,Y_train),one_hot_encoding=False)
-    r2_test_runs[i,:] = nn.r2_test
-    r2_train_runs[i,:] = nn.r2_train
-    r2_end_test[i] = nn.r2_test[-1]
-    r2_end_train[i] = nn.r2_train[-1]
+    r2_test_runs[run,:] = nn.r2_test
+    r2_train_runs[run,:] = nn.r2_train
+    r2_end_test[run] = nn.r2_test[-1]
+    r2_end_train[run] = nn.r2_train[-1]
 
 r2_mean_test = np.mean(r2_end_test)
 r2_mean_train = np.mean(r2_end_train)
-print('epochs',epochs,'runs',runs)
-print('eta ',eta,' lambda ',lmbd,' neuron list ',hidden_neuron_list)
-print('r2 mean test = ',r2_mean_test, ' r2 mean train = ',r2_mean_train)
 
 fig,ax = plt.subplots()
 for i in range(runs):
-    ax.plot(r2_test_runs[i,:],color='crimson',label='test')
-    ax.plot(r2_train_runs[i,:],color='navy',label='train')
+    ax.plot(r2_test_runs[i,:],color='crimson',label='test, mean = {:.2f}'.format(r2_mean_test))
+    ax.plot(r2_train_runs[i,:],color='navy',label='train, mean = {:.2f}'.format(r2_mean_train))
     if i == 0:
         ax.legend(loc=4)
 ax.set_ylabel('R2 score')
@@ -76,5 +73,13 @@ ax.set_xlabel('Epochs')
 ax.set_ylim(0,1.1)
 ax.axhline(y=1,linestyle='--',color='black')
 fig.tight_layout()
-fig.savefig('../figs/wh_r2_epochs_self.pdf')
+
+print('epochs',epochs,'runs',runs)
+print('eta ',eta,' lambda ',lmbd,' neuron list ',hidden_neuron_list)
+print('r2 mean test = ',r2_mean_test, ' r2 mean train = ',r2_mean_train)
+saving = True
+if saving:
+    filename = '../figs/wh_r2_epochs_self_eta_{:.3e}_lmbd_{:.3e}.pdf'.format(eta,lmbd)
+    print('saving figure to '+filename)
+    fig.savefig(filename)
 plt.show()
